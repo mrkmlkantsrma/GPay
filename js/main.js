45eca2411ddf839e545415f75a503362d2fb19f9
mrkmlkantsrma <kmlkantsrma@gmail.com>
2022-06-22 21:19:41 +0530

    push-rebuild

--- js/main.js
+++ js/main.js
@@ -1,71 +1,161 @@
-/*!
- * Made by Tuhin Kanti Pal
- * Visit https://tu.hin.life
- */
-var parser = document.createElement('a');
+/* ===================================================================
+ * Count - Main JS
+ *
+ * ------------------------------------------------------------------- */
 
-var parserhostname = 'upier-pp.vercel.app';
-parser.href = new URLSearchParams(location.search);
+(function($) {
 
-function hideF() {
-    document.getElementById("outputd").style.display = "none";
-    document.getElementById("invalid").style.visibility = "hidden";
-}
-var amountset ='1';
-var vpaset='';
-function genLink() {
-    if (vpa.value.indexOf("@") != -1) {
-        document.getElementById("outputd").style.display = "block";
-        document.getElementById("inputd").style.display = "none";
-        document.getElementById("copiedtext").style.visibility = "hidden";
-        // var amountset = document.getElementById("amount").value;
-        vpaset = document.getElementById("vpa").value;
-        if (amountset == "") {
-            var linkset = "https://" + parserhostname + "/pay?vpa=" + vpaset;
-            document.getElementById("outlink").value = linkset;
-            document.getElementById("outlink").select();
-        } else {
-            var linkset = "https://" + parserhostname + "/pay?vpa=" + vpaset + "&amount=" + amountset;
-            document.getElementById("outlink").value = "";
-            document.getElementById("outlink").select();
+    "use strict";
+    
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+        mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
+    },
+
+    $WIN = $(window);
+
+    // Add the User Agent to the <html>
+    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+
+    // svg fallback
+    if (!Modernizr.svg) {
+        $(".home-logo img").attr("src", "images/logo.png");
+    }
+
+
+   /* Preloader
+    * -------------------------------------------------- */
+    var ssPreloader = function() {
+        
+        $("html").addClass('ss-preload');
+
+        $WIN.on('load', function() {
+
+            // will first fade out the loading animation 
+            $("#loader").fadeOut("slow", function() {
+                // will fade out the whole DIV that covers the website.
+                $("#preloader").delay(300).fadeOut("slow");
+            }); 
+            
+            // for hero content animations 
+            $("html").removeClass('ss-preload');
+            $("html").addClass('ss-loaded');
+        
+        });
+    };
+
+
+   /* info toggle
+    * ------------------------------------------------------ */
+    var ssInfoToggle = function() {
+
+        //open/close lateral navigation
+        $('.info-toggle').on('click', function(event) {
+
+            event.preventDefault();
+            $('body').toggleClass('info-is-visible');
+
+        });
+
+    };
+
+
+   /* slick slider
+    * ------------------------------------------------------ */
+    var ssSlickSlider = function() {
+        
+        $('.home-slider').slick({
+            arrows: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            fade: true,
+            speed: 3000
+        });
+
+    };
+
+
+   /* placeholder plugin settings
+    * ------------------------------------------------------ */
+    var ssPlaceholder = function() {
+        $('input, textarea, select').placeholder();
+    };
+
+
+   /* final countdown
+    * ------------------------------------------------------ */
+    var ssFinalCountdown = function() {
+
+        var finalDate =  new Date("March 25, 2021 15:37:25").getTime();
+        //-date: "Mar 25 2021",
+
+        $('.home-content__clock').countdown(finalDate)
+        .on('update.countdown finish.countdown', function(event) {
+
+            var str = '<div class=\"top\"><div class=\"time days\">' +
+                      '%D <span>day%!D</span>' + 
+                      '</div></div>' +
+                      '<div class=\"time hours\">' +
+                      '%H <span>H</span></div>' +
+                      '<div class=\"time minutes\">' +
+                      '%M <span>M</span></div>' +
+                      '<div class=\"time seconds\">' +
+                      '%S <span>S</span></div>';
+
+            $(this)
+            .html(event.strftime(str));
+
+        });
+    };
+
+
+   /* AjaxChimp
+    * ------------------------------------------------------ */
+    var ssAjaxChimp = function() {
+        
+        $('#mc-form').ajaxChimp({
+            language: 'es',
+            url: cfg.mailChimpURL
+        });
+
+        // Mailchimp translation
+        //
+        //  Defaults:
+        //	 'submit': 'Submitting...',
+        //  0: 'We have sent you a confirmation email',
+        //  1: 'Please enter a value',
+        //  2: 'An email address must contain a single @',
+        //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+        //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+        //  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+        $.ajaxChimp.translations.es = {
+            'submit': 'Submitting...',
+            0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
+            1: '<i class="fas fa-exclamation-triangle"></i> You must enter a valid e-mail address.',
+            2: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            3: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            4: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            5: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.'
         }
-        // https://upier-pp.vercel.app/api?url=upi://pay?pn=kmlkantsrma-1@oksbi~tn=UPIER~pa=kmlkantsrma-1@oksbi~cu=INR~am=1
-        var payWith = 'https://'+parserhostname+'/api?url=upi://pay?pn='+vpaset+'~tn=KAMAL~pa='+vpaset+'~cu=INR~am='+amountset;
-        var UpiToPay = 'upi://pay?pn='+vpaset+'&tn=KAMAL&pa='+vpaset+'&cu=INR&am='+amountset;
-        document.getElementById("qrcodescan").src = payWith;
-        document.getElementById("upibtn").href = UpiToPay;
-        document.getElementById("holder").innerHTML = vpaset;
-    } else {
-        document.getElementById("invalid").style.visibility = "visible";
-        document.getElementById("vpa").value = "";
-    }
-}
-
-function payLink() {
-    // https://upier-pp.vercel.app/api?url=upi://pay?pn=kmlkantsrma-1@oksbi~tn=KAMAL~pa=kmlkantsrma-1@oksbi~cu=INR~am=1
-    document.getElementById("qrcode1").src = 'https://upier-pp.vercel.app/api?url=upi://pay?pn='+vpaset+'~tn=KAMAL~pa='+vpaset+'~cu=INR~am='+amountset;
-}
-
-function share() {
-    if (navigator.share) {
-        navigator.share({
-                title: 'Share | Upier',
-                text: "*Upier* \n\nShareable Secure Payment's Link for UPI :\n",
-                url: window.location.href
-            }).then(() => {
-                console.log('Thanks for sharing!');
-            })
-            .catch(err => {
-                console.log(`Couldn't share because of`, err.message);
-            });
-    } else {
-        console.log('web share not supported');
-    }
-}
+    };
 
 
+   /* initialize
+    * ------------------------------------------------------ */
+    (function ssInit() {
+        
+        ssPreloader();
+        ssInfoToggle();
+        ssSlickSlider();
+        ssPlaceholder();
+        ssFinalCountdown();
+        ssAjaxChimp();
 
-/*!
- * Made by Tuhin Kanti Pal
- * Visit https://tu.hin.life
- */
+    })();
+
+
+})(jQuery);
---
+++ js/pay-main.js
@@ -0,0 +1,71 @@
+/*!
+ * Made by Tuhin Kanti Pal
+ * Visit https://tu.hin.life
+ */
+var parser = document.createElement('a');
+
+var parserhostname = 'upier-pp.vercel.app';
+parser.href = new URLSearchParams(location.search);
+
+function hideF() {
+    document.getElementById("outputd").style.display = "none";
+    document.getElementById("invalid").style.visibility = "hidden";
+}
+var amountset ='1';
+var vpaset='';
+function genLink() {
+    if (vpa.value.indexOf("@") != -1) {
+        document.getElementById("outputd").style.display = "block";
+        document.getElementById("inputd").style.display = "none";
+        document.getElementById("copiedtext").style.visibility = "hidden";
+        // var amountset = document.getElementById("amount").value;
+        vpaset = document.getElementById("vpa").value;
+        if (amountset == "") {
+            var linkset = "https://" + parserhostname + "/pay?vpa=" + vpaset;
+            document.getElementById("outlink").value = linkset;
+            document.getElementById("outlink").select();
+        } else {
+            var linkset = "https://" + parserhostname + "/pay?vpa=" + vpaset + "&amount=" + amountset;
+            document.getElementById("outlink").value = "";
+            document.getElementById("outlink").select();
+        }
+        // https://upier-pp.vercel.app/api?url=upi://pay?pn=kmlkantsrma-1@oksbi~tn=UPIER~pa=kmlkantsrma-1@oksbi~cu=INR~am=1
+        var payWith = 'https://'+parserhostname+'/api?url=upi://pay?pn='+vpaset+'~tn=UPIER~pa='+vpaset+'~cu=INR~am='+amountset;
+        var UpiToPay = 'upi://pay?pn='+vpaset+'&tn=UPIER&pa='+vpaset+'&cu=INR&am='+amountset;
+        document.getElementById("qrcodescan").src = payWith;
+        document.getElementById("upibtn").href = UpiToPay;
+        document.getElementById("holder").innerHTML = vpaset;
+    } else {
+        document.getElementById("invalid").style.visibility = "visible";
+        document.getElementById("vpa").value = "";
+    }
+}
+
+function payLink() {
+    // https://upier-pp.vercel.app/api?url=upi://pay?pn=kmlkantsrma-1@oksbi~tn=UPIER~pa=kmlkantsrma-1@oksbi~cu=INR~am=1
+    document.getElementById("qrcode1").src = 'https://upier-pp.vercel.app/api?url=upi://pay?pn='+vpaset+'~tn=UPIER~pa='+vpaset+'~cu=INR~am='+amountset;
+}
+
+function share() {
+    if (navigator.share) {
+        navigator.share({
+                title: 'Share | Upier',
+                text: "*Upier* \n\nShareable Secure Payment's Link for UPI :\n",
+                url: window.location.href
+            }).then(() => {
+                console.log('Thanks for sharing!');
+            })
+            .catch(err => {
+                console.log(`Couldn't share because of`, err.message);
+            });
+    } else {
+        console.log('web share not supported');
+    }
+}
+
+
+
+/*!
+ * Made by Tuhin Kanti Pal
+ * Visit https://tu.hin.life
+ */
